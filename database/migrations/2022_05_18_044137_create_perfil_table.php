7141dcd90d0e2717eb9f979fd6f6eaeea2281ccf
Gonzalo Vargas <gonzalo.vargas@tecsup.edu.pe>
2022-05-27 01:49:17 -0500

    vistas y modelos

---
+++ database/migrations/2022_05_18_044137_create_perfil_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfil', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario');
+            $table->string('foto');
+            $table->string('nombrecompleto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfil');
+    }
+}
